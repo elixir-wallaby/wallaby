32a96b0820463254fde8632bb57add4ef6d4f230
Alex Daniel <adaniel@carbonfive.com>
2016-03-21 09:35:51 -0700

    Remove pending tags

--- test/wallaby/dsl_test.exs
+++ test/wallaby/dsl_test.exs
@@ -131,7 +131,6 @@ defmodule Wallaby.DSLTest do
     assert has_value?(node, "")
   end
 
-  @tag :pending
   test "choosing a radio button", %{session: session, server: server} do
     page =
       session
@@ -145,7 +144,6 @@ defmodule Wallaby.DSLTest do
     assert find(session, "#option2") |> checked?
   end
 
-  @tag :pending
   test "choosing a radio button unchecks other buttons in the group", %{session: session, server: server} do
     session
     |> visit(server.base_url <> "forms.html")
