f63dcd93dda1e65c93e4816b212de9bc25b731c2
Chris Keathley <c@keathley.io>
2016-04-23 16:29:40 -0400

    Raise Exception is there is no base_url set

--- lib/wallaby/exceptions.ex
+++ lib/wallaby/exceptions.ex
@@ -13,3 +13,21 @@ end
 defmodule Wallaby.BadMetadata do
   defexception [:message]
 end
+
+defmodule Wallaby.NoBaseUrl do
+  defexception [:message]
+
+  def exception(relative_path) do
+    msg = """
+    You called visit with #{relative_path}, but did not set a base_url.
+    Set this in config/test.exs or in test/test_helper.exs:
+
+      Application.put_env(:wallaby, :base_url, "http://localhost:4001")
+
+    If using Phoenix, you can use the url from your endpoint:
+
+      Application.put_env(:wallaby, :base_url, YourApplication.Endpoint.url)
+    """
+    %__MODULE__{message: msg}
+  end
+end
