58416b6afc1033b2dd3494994459a09b164a09d4
willie <willie@substantial.com>
2016-08-25 15:42:03 -0700

    Create a temp dir for each PhantomJS instance to use as local storage

--- test/wallaby/server_test.exs
+++ test/wallaby/server_test.exs
@@ -3,8 +3,30 @@ defmodule Wallaby.ServerTest do
 
   alias Wallaby.Server
 
-  test "it can start a server" do
+  setup do
     {:ok, server} = Server.start_link([])
+
+    local_storage = Server.get_local_storage_dir(server)
+    on_exit fn ->
+      File.rm_rf(local_storage)
+    end
+
+    {:ok, server: server}
+  end
+
+  test "it can start a server", %{server: server}  do
     assert Server.get_base_url(server) =~ ~r"http://localhost:\d+/"
+  end
+
+  test "separate servers do not share local storage", %{server: server} do
+    {:ok, other_server} = Server.start_link([])
+
+    local_storage = Server.get_local_storage_dir(server)
+    other_local_storage = Server.get_local_storage_dir(other_server)
+
+    # Remove the other directory before asserting so we can ensure deletion is successful
+    File.rm_rf(other_local_storage)
+
+    assert local_storage != other_local_storage
   end
 end
