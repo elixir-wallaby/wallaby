0805a5d71d4b44d6d740abe5ed0280a60151152d
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-03-22 20:08:53 +0100

    test phantomjs_args are calculated for being passed to script

--- test/wallaby/phantom/configure_test.exs
+++ test/wallaby/phantom/configure_test.exs
@@ -19,4 +19,34 @@ defmodule Wallaby.Phantom.ConfigrationTest do
       assert "test/path/phantomjs" in Wallaby.Phantom.Server.script_args(1234, '/tmp/dir')
     end
   end
+
+  describe "adding phantom args" do
+    setup do
+      old_env = Application.get_env(:wallaby, :phantomjs_args)
+      context = %{old_env: old_env}
+
+      on_exit fn ->
+        reset_env(context)
+      end
+
+      {:ok, context}
+    end
+
+    defp reset_env(%{old_env: old_env}) do
+      Application.put_env(:wallaby, :phantomjs_args, old_env)
+    end
+
+    test "updates the phantomjs command", context do
+      options = [opt1, opt2] = ["--some-opt=value", "--other-opt"]
+
+      Application.put_env(:wallaby, :phantomjs_args, Enum.join(options, " "))
+      assert opt1 in Wallaby.Phantom.Server.script_args(1234, '/tmp/dir')
+      assert opt2 in Wallaby.Phantom.Server.script_args(1234, '/tmp/dir')
+
+      reset_env(context)
+      Application.put_env(:wallaby, :phantomjs_args, options)
+      assert opt1 in Wallaby.Phantom.Server.script_args(1234, '/tmp/dir')
+      assert opt2 in Wallaby.Phantom.Server.script_args(1234, '/tmp/dir')
+    end
+  end
 end
