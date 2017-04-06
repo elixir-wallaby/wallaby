2294ccd7d9d141e26e230855afa0550682707f81
Aaron Renner <aaron.renner@gmail.com>
2017-04-06 13:01:16 -0600

    Test to ensure StaleReferenceException is raised

--- test/wallaby/browser/stale_nodes_test.exs
+++ test/wallaby/browser/stale_nodes_test.exs
@@ -10,5 +10,18 @@ defmodule Wallaby.Browser.StaleElementsTest do
 
       assert element
     end
+
+    test "when a DOM element disappears", %{session: session} do
+      element =
+        session
+        |> visit("stale_nodes.html")
+        |> find(Query.css("#removed-node"))
+
+      Process.sleep(400) # let the node disappear via javascript
+
+      assert_raise Wallaby.StaleReferenceException, fn ->
+        Element.value(element)
+      end
+    end
   end
 end
