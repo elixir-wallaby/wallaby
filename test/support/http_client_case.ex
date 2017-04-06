858767d8c45ba49347eb854d810aed6545835423
Aaron Renner <aaron.renner@gmail.com>
2017-04-05 20:49:55 -0600

    Added tests for Wallaby.Phantom.Driver (#191)
    * Added tests for Wallaby.Phantom.Driver
    
    This change tests the interactions between the Wallaby.Phantom.Driver
    module and the PhantomJS webdriver API. I wanted to make sure I had test
    coverage here so I didn't accidentally break the
    serialization/deserialization logic when I start extracting logic to add
    a Selenium driver.
    
    Another couple of changes to note:
    
      * Changed the Content-Type header from "text/json" to
        "application/json". See: http://stackoverflow.com/a/477819
      * Extracted a create_session/2 function in Wallaby.Phantom.Driver that
        only handles the API interaction. This made it much easier to test
        with Bypass. I also used @doc false because I didn't want to expose
        a new public function in the docs.
    
    * Removed unnecessary comments

---
+++ test/support/http_client_case.ex
@@ -0,0 +1,35 @@
+defmodule Wallaby.HttpClientCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Plug.Conn
+      import Wallaby.HttpClientCase
+
+      setup [:start_bypass]
+    end
+  end
+
+  @doc """
+  Starts a bypass session and inserts it into the test session
+  """
+  def start_bypass(_) do
+    {:ok, bypass: Bypass.open}
+  end
+
+  @doc """
+  Builds a url from the current bypass session
+  """
+  def bypass_url(bypass), do: "http://localhost:#{bypass.port}"
+  def bypass_url(bypass, path) do
+    "#{bypass_url(bypass)}#{path}"
+  end
+
+  @doc """
+  Parses the body of an incoming http request
+  """
+  def parse_body(conn) do
+    opts = Plug.Parsers.init(parsers: [:urlencoded, :json], json_decoder: Poison)
+    Plug.Parsers.call(conn, opts)
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,3 +3,5 @@ Application.put_env(:wallaby, :base_url, server.base_url)
 
 ExUnit.configure(exclude: [pending: true])
 ExUnit.start()
+
+Application.ensure_all_started(:bypass)
