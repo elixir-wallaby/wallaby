8fd294ef290293c0ca4060c45fcd434fa6c3f890
Aaron Renner <aaron.renner@gmail.com>
2017-04-18 14:35:22 -0600

    Make SessionCase.start_test_session easier to read

--- integration_test/support/session_case.ex
+++ integration_test/support/session_case.ex
@@ -22,13 +22,16 @@ defmodule Wallaby.Integration.SessionCase do
   Starts a test session with the default opts for the given driver
   """
   def start_test_session(opts \\ []) do
-    session_opts = case System.get_env("WALLABY_DRIVER") do
-      "phantom" ->
-        []
-      other ->
-        raise "Unknown value for WALLABY_DRIVER environment variable: #{other}"
-    end |> Keyword.merge(opts)
+    session_opts =
+      System.get_env("WALLABY_DRIVER")
+      |> default_opts_for_driver
+      |> Keyword.merge(opts)
 
     Wallaby.start_session(session_opts)
+  end
+
+  defp default_opts_for_driver("phantom"), do: []
+  defp default_opts_for_driver(other) do
+    raise "Unknown value for WALLABY_DRIVER environment variable: #{other}"
   end
 end
