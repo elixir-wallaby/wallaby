9813b7098368ec0a8949cf319db49748a315135e
Graeme Defty <graeme.defty@gmail.com>
2017-05-10 17:50:32 +0700

    Remove deprecated attr/2

--- integration_test/cases/browser/attr_test.exs
+++ integration_test/cases/browser/attr_test.exs
@@ -1,16 +1,6 @@
 defmodule Wallaby.Integration.Browser.AttrTest do
   use Wallaby.Integration.SessionCase, async: true
 
-  test "can get attributes of an element", %{session: session} do
-    class =
-      session
-      |> visit("/")
-      |> find(Query.css("body"))
-      |> attr("class")
-
-    assert class =~ "bootstrap"
-  end
-
   test "can get the attributes of a query", %{session: session} do
     class =
       session
