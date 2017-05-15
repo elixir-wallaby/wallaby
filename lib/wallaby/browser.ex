14be461bb171fad99136e3d5a1da7aaeff4b8d7c
Graeme Defty <graeme.defty@gmail.com>
2017-05-15 23:29:16 +0700

    Update set_value documentation

--- lib/wallaby/browser.ex
+++ lib/wallaby/browser.ex
@@ -390,16 +390,14 @@ defmodule Wallaby.Browser do
   end
 
   @doc """
-  Sets the value of an element.
+  Sets the value of an element. The allowed type for the value depends on the
+  type of the element. The value may be:
+  * a string of characters for a text element
+  * :selected for a radio button, checkbox or select list option
+  * :unselected for a checkbox
   """
   @spec set_value(parent, Query.t, Element.value) :: parent
-#  @spec set_value(element, any()) :: element
 
-#  def set_value(element, value) do
-#    IO.warn "set_value/2 has been deprecated. Please use Element.set_value/2"
-#
-#    Element.set_value(element, value)
-#  end
   def set_value(parent, query, :selected) do
     find(parent, query, fn(element) ->
       case Element.selected?(element) do
