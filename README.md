5c60740338f456f747cd14dba91f929e890d660b
Chris Keathley <c@keathley.io>
2017-08-08 12:44:16 -0400

    Bump version.

--- README.md
+++ README.md
@@ -71,7 +71,7 @@ Add Wallaby to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:wallaby, "~> 0.18.1"}]
+  [{:wallaby, "~> 0.19.0"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Wallaby.Mixfile do
   use Mix.Project
 
-  @version "0.18.1"
+  @version "0.19.0"
   @drivers ~w(phantom selenium chrome)
   @selected_driver System.get_env("WALLABY_DRIVER")
   @maintainers [
