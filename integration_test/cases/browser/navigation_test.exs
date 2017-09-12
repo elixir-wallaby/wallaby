8dd5124130b98739d2f620c7824c362093d8bc73
Aaron Renner <aaron.renner@gmail.com>
2017-09-11 21:54:46 -0600

    Fixes visit/2 issue where urls have too many slashes
    I noticed in #300 that visit/2 is generating urls like
    http://localhost:54579//logs.html that have too many slashes. This
    occurs when the base_url has a trailing slash and the requested path
    has a leading slash. This change adds tests and fixes the issue so we
    don't end up with urls with too many or not enough slashes
    depending on how these settings are configured.

--- integration_test/cases/browser/navigation_test.exs
+++ integration_test/cases/browser/navigation_test.exs
@@ -11,18 +11,6 @@ defmodule Wallaby.Integration.Browser.NavigationTest do
     assert element
   end
 
-  test "visit/2 with a relative url and no base url raises exception", %{session: session} do
-    original_url = Application.get_env(:wallaby, :base_url)
-
-    assert_raise(Wallaby.NoBaseUrl, fn ->
-      Application.put_env(:wallaby, :base_url, nil)
-      session
-      |> visit("/page_1.html")
-    end)
-
-    Application.put_env(:wallaby, :base_url, original_url)
-  end
-
   test "visit/2 with an absolute path does not use the base url", %{session: session} do
     session
     |> visit("/page_1.html")
---
+++ test/support/settings_test_helpers.ex
@@ -0,0 +1,21 @@
+defmodule Wallaby.SettingsTestHelpers do
+  @moduledoc """
+  Test helpers for working with app environments
+  """
+
+  import ExUnit.Callbacks, only: [on_exit: 1]
+
+  @doc """
+  Ensures a setting is reset for the app's environment after the test is run.
+  """
+  def ensure_setting_is_reset(app, setting) do
+    orig_result = Application.fetch_env(app, setting)
+
+    on_exit fn -> reset_env(orig_result, app, setting) end
+  end
+
+  defp reset_env({:ok, orig}, app, setting) do
+    Application.put_env(app, setting, orig)
+  end
+  defp reset_env(:error, app, setting), do: Application.delete_env(app, setting)
+end
