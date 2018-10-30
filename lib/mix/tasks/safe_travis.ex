9336c5cbd04395ffa06a435513edf13b7667deca
Mitchell Hanberg <mitch@mitchellhanberg.com>
2018-10-29 22:59:17 -0400

    Fix indenting of coveralls.safe_travis mix task

--- lib/mix/tasks/safe_travis.ex
+++ lib/mix/tasks/safe_travis.ex
@@ -1,15 +1,16 @@
 defmodule Mix.Tasks.Coveralls.SafeTravis do
-   @moduledoc false
+  @moduledoc false
+  alias Mix.Tasks.Coveralls
 
-   use Mix.Task
+  use Mix.Task
 
-   @preferred_cli_env :test
-   @shortdoc "A safe `coveralls.travis` variant that doesn't crash on failed upload."
+  @preferred_cli_env :test
+  @shortdoc "A safe `coveralls.travis` variant that doesn't crash on failed upload."
 
-   def run(args) do
-     Mix.Tasks.Coveralls.do_run(args, type: "travis")
-   rescue
-     e in ExCoveralls.ReportUploadError ->
-       Mix.shell().error("Failed coveralls upload: #{Exception.message(e)}")
-   end
- end
+  def run(args) do
+    Coveralls.do_run(args, type: "travis")
+  rescue
+    e in ExCoveralls.ReportUploadError ->
+      Mix.shell().error("Failed coveralls upload: #{Exception.message(e)}")
+  end
+end
