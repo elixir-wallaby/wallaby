7dafddde1847509fde4d88c269dfc5d04c643f0d
Mitchell Hanberg <mitch@mitchellhanberg.com>
2018-11-15 14:04:52 -0500

    Print outerHTML when passing an Element to IO.inspect/2 (#396)

---
+++ integration_test/cases/inspect_test.exs
@@ -0,0 +1,39 @@
+defmodule Wallaby.Integration.InspectTest do
+  use Wallaby.Integration.SessionCase, async: true
+  import ExUnit.CaptureIO
+
+  describe "inspect/2" do
+    test "prints the outerHTML of the element", %{session: session} do
+      expected = """
+      #{IO.ANSI.cyan}outerHTML:
+
+      #{IO.ANSI.reset}#{IO.ANSI.yellow}<body class="bootstrap index-page">
+        <h1 id="header">Test Index</h1>
+        <ul>
+          <li><a href="page_1.html">Page 1</a></li>
+          <li><a href="page_2.html">Page 2</a></li>
+          <li><a href="page_3.html">Page 3</a></li>
+        </ul>
+
+        <div id="parent">
+          The Parent
+          <div id="child">
+            The Child
+          </div>
+        </div>
+      </body>#{IO.ANSI.reset}
+      """
+      |> String.replace(~r/\s/, "")
+
+      actual = capture_io(fn ->
+        session
+        |> visit("/index.html")
+        |> find(Query.css("body"))
+        |> IO.inspect
+      end)
+      |> String.replace(~r/\s/, "")
+
+      assert actual =~ expected
+    end
+  end
+end
