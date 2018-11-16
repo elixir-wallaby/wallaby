d2763c5637a41c2ddd7b8b3a55eacb0eec89d590
Mitchell Hanberg <mitch@mitchellhanberg.com>
2018-11-15 19:09:21 -0500

    Find elements by text that contain single quotes (#397)

--- lib/wallaby/query/xpath.ex
+++ lib/wallaby/query/xpath.ex
@@ -75,7 +75,7 @@ defmodule Wallaby.Query.XPath do
   Matches any element by its inner text.
   """
   def text(selector) do
-    ~s{.//*[contains(normalize-space(text()), '#{selector}')]}
+    ~s{.//*[contains(normalize-space(text()), "#{selector}")]}
   end
 
   @doc """
