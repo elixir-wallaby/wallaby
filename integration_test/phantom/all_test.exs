3c20eb488c5bdb515ad7418fe1454ada977a4ff1
Michał Łępicki <michal.lepicki@gmail.com>
2019-05-08 14:14:16 +0200

    Enable screenshot support for Selenium

--- integration_test/phantom/all_test.exs
+++ integration_test/phantom/all_test.exs
@@ -4,4 +4,3 @@ Code.require_file "../tests.exs", __DIR__
 Code.require_file "../cases/browser/dialog_test.exs", __DIR__
 Code.require_file "../cases/browser/file_test.exs", __DIR__
 Code.require_file "../cases/browser/js_errors_test.exs", __DIR__
-Code.require_file "../cases/browser/screenshot_test.exs", __DIR__
