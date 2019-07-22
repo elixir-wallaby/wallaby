fbe7f5cb29b0f8941ec9aae94b131d43c7dfd5fe
Michał Łępicki <michal.lepicki@gmail.com>
2019-07-22 15:53:18 +0200

    Enable running hover integration tests added in #435

--- integration_test/selenium/all_test.exs
+++ integration_test/selenium/all_test.exs
@@ -1,1 +1,5 @@
 Code.require_file "../tests.exs", __DIR__
+
+# Additional test cases supported by selenium
+Code.require_file "../cases/browser/hover_test.exs", __DIR__
+Code.require_file "../cases/element/hover_test.exs", __DIR__
