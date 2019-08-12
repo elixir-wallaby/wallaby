29b1474cecfebaaf7e56b60a5b8fbe542848b394
Michał Łępicki <michal.lepicki@gmail.com>
2019-08-12 14:36:21 +0200

    test name fixup

--- integration_test/chrome/all_test.exs
+++ integration_test/chrome/all_test.exs
@@ -5,8 +5,8 @@ Code.require_file "../cases/browser/file_test.exs", __DIR__
 Code.require_file "../cases/browser/js_errors_test.exs", __DIR__
 Code.require_file "../cases/browser/click_mouse_button_test.exs", __DIR__
 Code.require_file "../cases/browser/double_click_test.exs", __DIR__
-Code.require_file "../cases/browser/button_down.exs", __DIR__
-Code.require_file "../cases/browser/button_up.exs", __DIR__
+Code.require_file "../cases/browser/button_down_test.exs", __DIR__
+Code.require_file "../cases/browser/button_up_test.exs", __DIR__
 Code.require_file "../cases/browser/hover_test.exs", __DIR__
 Code.require_file "../cases/element/hover_test.exs", __DIR__
 Code.require_file "../cases/browser/move_mouse_by_test.exs", __DIR__
