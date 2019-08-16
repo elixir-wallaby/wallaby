1b7b257baa7e0a763ae67eea8095099b52c7d362
Jan Gromko <jan.gromko@amberbit.com>
2019-08-16 12:58:31 +0200

    Move test for sending text to active element to separate files and enable it for chromedriver

--- integration_test/cases/browser/send_text_test.exs
+++ integration_test/cases/browser/send_text_test.exs
@@ -28,19 +28,5 @@ defmodule Wallaby.Integration.Browser.SendTextTest do
       |> send_keys("Example text")
       |> has_value?("Example text")
     end
-
-    test "allows to send text to the active element", %{page: page} do
-      page
-      |> click(Query.text_field("Name"))
-      |> send_keys(["Chris", :tab, "c@keathley.io"])
-
-      assert page
-      |> find(Query.text_field("Name"))
-      |> has_value?("Chris")
-
-      assert page
-      |> find(Query.text_field("email"))
-      |> has_value?("c@keathley.io")
-    end
   end
 end
