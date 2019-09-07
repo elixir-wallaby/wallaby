e99e549ad0ba5a2cb89937cae0ab5441efd3d2f4
Jan Gromko <jan.gromko@amberbit.com>
2019-09-07 15:24:15 +0200

    Tests added to Chrome and Selenium test sets

--- integration_test/selenium/all_test.exs
+++ integration_test/selenium/all_test.exs
@@ -7,3 +7,13 @@ Code.require_file "../cases/element/hover_test.exs", __DIR__
 Code.require_file "../cases/browser/move_mouse_by_test.exs", __DIR__
 Code.require_file "../cases/browser/window_handles_test.exs", __DIR__
 Code.require_file "../cases/browser/window_position_test.exs", __DIR__
+Code.require_file "../cases/browser/touch_down_test.exs", __DIR__
+Code.require_file "../cases/element/touch_down_test.exs", __DIR__
+Code.require_file "../cases/browser/touch_up_test.exs", __DIR__
+Code.require_file "../cases/browser/tap_test.exs", __DIR__
+Code.require_file "../cases/element/tap_test.exs", __DIR__
+Code.require_file "../cases/browser/touch_scroll_test.exs", __DIR__
+Code.require_file "../cases/element/touch_scroll_test.exs", __DIR__
+Code.require_file "../cases/browser/touch_move_test.exs", __DIR__
+Code.require_file "../cases/element/size_test.exs", __DIR__
+Code.require_file "../cases/element/location_test.exs", __DIR__
