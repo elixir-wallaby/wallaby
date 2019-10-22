495ba845612c4b66a074b0a1b9903d4991b0a0a0
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-10-22 19:54:27 -0400

    Revert changes to travis and set ecto and phx dependencies to runtime false

--- mix.exs
+++ mix.exs
@@ -61,8 +61,8 @@ defmodule Wallaby.Mixfile do
       {:excoveralls, "~> 0.7",  only: :test},
       {:ex_doc, "~> 0.20", only: :docs},
       {:inch_ex, "~> 0.5", only: :docs},
-      {:ecto_sql, "~> 3.0.0", only: [:dev, :test]},
-      {:phoenix_ecto, "~> 4.0.0", only: [:dev, :test]},
+      {:ecto_sql, "~> 3.0.0", only: [:dev, :test], runtime: false},
+      {:phoenix_ecto, "~> 4.0.0", only: [:dev, :test], runtime: false},
     ]
   end
 
