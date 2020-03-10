e495ff8ef971b8f83533e3bbb10ec570613cffd3
Mitchell Hanberg <mitch@mitchellhanberg.com>
2020-03-10 18:16:18 -0400

    Don't define a ton of functions in the Feature.__using_macro

--- lib/wallaby/feature.ex
+++ lib/wallaby/feature.ex
@@ -1,4 +1,6 @@
 defmodule Wallaby.Feature do
+  @includes_ecto Code.ensure_loaded?(Ecto.Adapters.SQL.Sandbox) &&
+                   Code.ensure_loaded?(Phoenix.Ecto.SQL.Sandbox)
   @moduledoc """
   Helpers for writing features.
 
@@ -23,11 +25,7 @@ defmodule Wallaby.Feature do
       import Wallaby.Feature
 
       setup context do
-        metadata =
-          otp_app()
-          |> ecto_repos()
-          |> Enum.map(&checkout_ecto_repos(&1, context[:async]))
-          |> metadata_for_ecto_repos()
+        metadata = configure_ecto()
 
         start_session_opts =
           [metadata: metadata]
@@ -40,53 +38,70 @@ defmodule Wallaby.Feature do
 
         sessions |> build_setup_return()
       end
+    end
+  end
 
-      defp build_setup_return([session] = sessions) when length(sessions) == 1 do
-        [session: session]
+  def build_setup_return([session] = sessions) when length(sessions) == 1 do
+    [session: session]
+  end
+
+  def build_setup_return(sessions) do
+    [sessions: sessions]
+  end
+
+  def sessions_iterable(nil), do: 1..1
+  def sessions_iterable(count) when is_number(count), do: 1..count
+  def sessions_iterable(capabilities) when is_list(capabilities), do: capabilities
+
+  def start_session(more_opts, start_session_opts) when is_list(more_opts) do
+    {:ok, session} =
+      start_session_opts
+      |> Keyword.merge(more_opts)
+      |> Wallaby.start_session()
+
+    session
+  end
+
+  def start_session(num, start_session_opts) when is_number(num) do
+    {:ok, session} = Wallaby.start_session(start_session_opts)
+
+    session
+  end
+
+  def put_create_session_fn(opts, nil), do: opts
+  def put_create_session_fn(opts, func), do: Keyword.put(opts, :create_session_fn, func)
+
+  if @includes_ecto do
+    def otp_app(), do: Application.get_env(:wallaby, :otp_app)
+    def ecto_repos(nil), do: []
+    def ecto_repos(otp_app), do: Application.get_env(otp_app, :ecto_repos, [])
+
+    def checkout_ecto_repos(repo, async) do
+      :ok = Ecto.Adapters.SQL.Sandbox.checkout(repo)
+
+      unless async, do: Ecto.Adapters.SQL.Sandbox.mode(repo, {:shared, self()})
+
+      repo
+    end
+
+    def metadata_for_ecto_repos([]), do: Map.new()
+
+    def metadata_for_ecto_repos(repos) do
+      Phoenix.Ecto.SQL.Sandbox.metadata_for(repos, self())
+    end
+  end
+
+  defmacro configure_ecto do
+    if @includes_ecto do
+      quote do
+        otp_app()
+        |> ecto_repos()
+        |> Enum.map(&checkout_ecto_repos(&1, context[:async]))
+        |> metadata_for_ecto_repos()
       end
-
-      defp build_setup_return(sessions) do
-        [sessions: sessions]
-      end
-
-      defp sessions_iterable(nil), do: 1..1
-      defp sessions_iterable(count) when is_number(count), do: 1..count
-      defp sessions_iterable(capabilities) when is_list(capabilities), do: capabilities
-
-      defp start_session(more_opts, start_session_opts) when is_list(more_opts) do
-        {:ok, session} =
-          start_session_opts
-          |> Keyword.merge(more_opts)
-          |> Wallaby.start_session()
-
-        session
-      end
-
-      defp start_session(num, start_session_opts) when is_number(num) do
-        {:ok, session} = Wallaby.start_session(start_session_opts)
-
-        session
-      end
-
-      defp put_create_session_fn(opts, nil), do: opts
-      defp put_create_session_fn(opts, func), do: Keyword.put(opts, :create_session_fn, func)
-
-      defp otp_app(), do: Application.get_env(:wallaby, :otp_app)
-      defp ecto_repos(nil), do: []
-      defp ecto_repos(otp_app), do: Application.get_env(otp_app, :ecto_repos, [])
-
-      defp checkout_ecto_repos(repo, async) do
-        :ok = Ecto.Adapters.SQL.Sandbox.checkout(repo)
-
-        unless async, do: Ecto.Adapters.SQL.Sandbox.mode(repo, {:shared, self()})
-
-        repo
-      end
-
-      defp metadata_for_ecto_repos([]), do: Map.new()
-
-      defp metadata_for_ecto_repos(repos) do
-        Phoenix.Ecto.SQL.Sandbox.metadata_for(repos, self())
+    else
+      quote do
+        ""
       end
     end
   end
