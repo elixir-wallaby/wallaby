0278a805d5bf735bbffb0a2c9254cf6215c82efa
Mitchell Hanberg <mitchell.hanberg@bleacherreport.com>
2020-04-07 20:42:15 -0400

    WIP

--- lib/wallaby/session_store.ex
+++ lib/wallaby/session_store.ex
@@ -6,12 +6,12 @@ defmodule Wallaby.SessionStore do
 
   def start_link, do: GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
 
-  def monitor(session), do: GenServer.call(__MODULE__, {:monitor, session}, 10_000)
+  def monitor(session) do
+    GenServer.call(__MODULE__, {:monitor, session}, 10_000)
+  end
 
-  def demonitor(session), do: GenServer.call(__MODULE__, {:demonitor, session})
-
-  def list_sessions_for(owner_pid \\ self()) when is_pid(owner_pid) do
-    GenServer.call(__MODULE__, {:list_sessions, owner_pid})
+  def demonitor(session) do
+    GenServer.call(__MODULE__, {:demonitor, session})
   end
 
   def init(:ok) do
@@ -21,13 +21,13 @@ defmodule Wallaby.SessionStore do
 
   def handle_call({:monitor, session}, {pid, _ref}, %{refs: refs} = state) do
     ref = Process.monitor(pid)
-    refs = Map.put(refs, ref, {pid, session})
+    refs = Map.put(refs, ref, session)
 
-    {:reply, :ok, %{state | refs: refs}}
+    {:reply, :ok, %{state | refs: refs} }
   end
 
   def handle_call({:demonitor, session}, _from, %{refs: refs} = state) do
-    case Enum.find(refs, fn {_, {_p, s}} -> s.id == session.id end) do
+    case Enum.find(refs, fn {_, s} -> s.id == session.id end) do
       {ref, _} ->
         {_, refs} = Map.pop(refs, ref)
         true = Process.demonitor(ref)
@@ -36,16 +36,6 @@ defmodule Wallaby.SessionStore do
       nil ->
         {:reply, :ok, state}
     end
-  end
-
-  def handle_call({:list_sessions, owner_pid}, _from, %{refs: refs} = state) do
-    sessions =
-      Enum.flat_map(refs, fn
-        {_ref, {^owner_pid, %Wallaby.Session{} = session}} -> [session]
-        _ -> []
-      end)
-
-    {:reply, sessions, state}
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, %{refs: refs} = state) do
