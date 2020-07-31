4b38c5a60e8e1b7e95a4df843fc44ffe0ff6f435
bobwaycott <bob@payitoff.io>
2020-07-31 18:14:50 -0400

    Add a couple simple tests to verify sessions are added and removed from the ETS table on monitor/demonitor calls, as expected

---
+++ test/wallaby/session_store_test.exs
@@ -0,0 +1,23 @@
+defmodule Wallaby.SessionStoreTest do
+  @moduledoc false
+  use ExUnit.Case
+  alias Wallaby.{Session, SessionStore}
+
+  describe "monitor/1" do
+    test "adds session to list of active sessions" do
+      assert [] = SessionStore.list_sessions_for()
+      {:ok, session} = Wallaby.driver().start_session([])
+      :ok = SessionStore.monitor(session)
+      assert [session] = SessionStore.list_sessions_for()
+    end
+  end
+
+  describe "demonitor/1" do
+    test "adds session to list of active sessions" do
+      {:ok, session} = Wallaby.driver().start_session([])
+      :ok = SessionStore.monitor(session)
+      :ok = SessionStore.demonitor(session)
+      assert [] = SessionStore.list_sessions_for()
+    end
+  end
+end
