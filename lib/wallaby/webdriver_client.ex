8f542d53ec4a4ff70d3c0b2316e22dc66ccb37c4
Bob Waycott <196710+bobwaycott@users.noreply.github.com>
2020-12-06 12:07:10 -0500

    Fix Selenium uploads (#567)
    * Have separate function heads for `send_keys/2` when `Session` or `Element` as first arg
    
    * For both local & remote Selenium server, we need to upload the file to Selenium server, which will return the path local to the server for the uploaded file, which can then correctly add that file to a file input & successfully upload the actual file. Prior functionality was only setting a value on the file input, which isn't the same thing as actually attaching a file.
    
    * doc change
    
    * doc update
    
    * Some small changes to handle multiple file uploads with Selenium, and improve local file checking without breaking `send_keys` when an actual list of key codes that aren't local files is provided
    
    * add tests for `KeyCodes.chars`

--- lib/wallaby/webdriver_client.ex
+++ lib/wallaby/webdriver_client.ex
@@ -437,7 +437,7 @@ defmodule Wallaby.WebdriverClient do
   @doc """
   Sends a list of key strokes to active element.
   """
-  @spec send_keys(Session.t(), [String.t() | atom]) :: {:ok, nil}
+  @spec send_keys(parent(), [String.t() | atom]) :: {:ok, any}
   def send_keys(%Session{} = session, keys) when is_list(keys) do
     with {:ok, resp} <-
            request(:post, "#{session.session_url}/keys", KeyCodes.json(keys), encode_json: false),
@@ -445,9 +445,9 @@ defmodule Wallaby.WebdriverClient do
          do: {:ok, value}
   end
 
-  def send_keys(parent, keys) when is_list(keys) do
+  def send_keys(%Element{} = element, keys) when is_list(keys) do
     with {:ok, resp} <-
-           request(:post, "#{parent.url}/value", KeyCodes.json(keys), encode_json: false),
+           request(:post, "#{element.url}/value", KeyCodes.json(keys), encode_json: false),
          {:ok, value} <- Map.fetch(resp, "value"),
          do: {:ok, value}
   end
--- test/wallaby/helpers/key_codes_test.exs
+++ test/wallaby/helpers/key_codes_test.exs
@@ -3,12 +3,25 @@ defmodule Wallaby.Helpers.KeyCodesTest do
 
   import Wallaby.Helpers.KeyCodes
 
-  test "encoding unicode values" do
+  test "encoding unicode values as JSON" do
     assert json([:enter]) == "{\"value\": [\"\\uE007\"]}"
     assert json([:shift, :enter]) == "{\"value\": [\"\\uE008\",\"\\uE007\"]}"
   end
 
-  test "encoding values with strings" do
+  test "encoding values with strings as JSON" do
     assert json(["te", :enter]) == "{\"value\": [\"t\",\"e\",\"\\uE007\"]}"
+  end
+
+  test "ensuring chars returns list of strings" do
+    assert chars([:enter]) == ["\\uE007"]
+    assert chars(:enter) == ["\\uE007"]
+    assert chars([:shift, :enter]) == ["\\uE008", "\\uE007"]
+    assert chars(["John", :tab, "Smith"]) == ["John", "\\uE004", "Smith"]
+  end
+
+  test "ensuring chars returns file paths as file paths" do
+    assert chars("/some/path/to/foo.txt") == ["/some/path/to/foo.txt"]
+    files = ["/path/to/foo.txt", "/path/to/bar.txt"]
+    assert chars(files) == files
   end
 end
