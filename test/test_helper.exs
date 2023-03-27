961b4cec4b364cc7a25922b204e5aa7ad4f8e66c
chriscrabtree <618489+chriscrabtree@users.noreply.github.com>
2023-03-27 07:17:45 -0500

    set Wallaby test concurrency same as user value
    Without this change, running mix test on Wallaby itself on a 56-core
    machine produces the same undesirable side effects we are trying to
    remedy with this PR.
    
    Specifically, all the CPU cores fire up, tests complete slowly (at
    least 45 seconds for a single test), and with many errors.
    
    We believe this is due to something related to how chromedriver
    operates, but the workaround of limiting concurrency to 10 processes
    works well enough for us to defer going down that rabbit hole.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,8 @@
-ExUnit.configure(exclude: [pending: true])
+ExUnit.configure(
+  exclude: [pending: true],
+  max_cases: min(System.schedulers_online(), 10)
+)
+
 EventEmitter.start_link([])
 
 ExUnit.start()
